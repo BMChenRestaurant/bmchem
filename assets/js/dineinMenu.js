527763934b6f873ebdeada8402013c308e7c077d
Eddyl112 <edwardlui17@gmail.com>
2023-01-14 18:41:58 -0500

    test

--- assets/js/dineinMenu.js
+++ assets/js/dineinMenu.js
@@ -1,12 +1,12 @@
 function parseMenu() {
-        Papa.parse("takeout.csv", {
+        Papa.parse("dinein.csv", {
           header: true,
           download: true,
           complete: function(results) {
 		    var curCol = 0;
             for (var i = 0; i < results.data.length; i++) {
               var menuItem = results.data[i];
-			  if(menuItem.price == "header" && (menuItem.item == "煲 Clay Pot" || menuItem.item == "海鲜类 Seafood" || menuItem.item == "Luncheon Specials" )) {
+			  if(menuItem.price == "header" && (menuItem.item == "煲 Clay Pot" || menuItem.item == "海鲜类 Seafood")) {
 			    curCol++;
 			  }
 			  var menuList = document.getElementById("menu-list-" + curCol.toString());
