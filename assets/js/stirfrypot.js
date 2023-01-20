a6b5daab254d501db9662996bd802387c5164c9e
Eddyl112 <edwardlui17@gmail.com>
2023-01-20 01:53:56 -0500

    Update stirfrypot.js

--- assets/js/stirfrypot.js
+++ assets/js/stirfrypot.js
@@ -3,25 +3,25 @@ function parseMenu() {
 		header: true,
 		download: true,
 		complete: function(results) {
-		var curCol = 0;
-		for (var i = 0; i < results.data.length; i++) {
-			var menuItem = results.data[i];
-			var menuList = document.getElementById("menu-list-" + curCol.toString());
-			var listItem = document.createElement("dl");
-			var sepItem = document.createElement("dt");
-			if(menuItem.price == "description") {
-                listItem.className = "desc";
-                listItem.innerHTML = "<p class=\"listing\">" + menuItem.item + "</p>";
-			} else if(menuItem.price == "header") {
-                sepItem.innerHTML = "<h3 class=\"header\">" + menuItem.item + "</h3>";
-                menuList.appendChild(sepItem);
-                continue;
-			} else {
-			    listItem.innerHTML = "<p class=\"listing\"><strong>" + menuItem.item + " - " + menuItem.price + "</strong></p>";
+			var curCol = 0;
+			for (var i = 0; i < results.data.length; i++) {
+				var menuItem = results.data[i];
+				var menuList = document.getElementById("menu-list-" + curCol.toString());
+				var listItem = document.createElement("dl");
+				var sepItem = document.createElement("dt");
+				console.log(menuItem.item);
+				if(menuItem.price == "description") {
+					listItem.className = "desc";
+					listItem.innerHTML = "<p class=\"listing\">" + menuItem.item + "</p>";
+				} else if(menuItem.price == "header") {
+					sepItem.innerHTML = "<h3 class=\"header\">" + menuItem.item + "</h3>";
+					menuList.appendChild(sepItem);
+					continue;
+				} else {
+					listItem.innerHTML = "<p class=\"listing\"><strong>" + menuItem.item + " - " + menuItem.price + "</strong></p>";
+				}
+				menuList.appendChild(listItem);
 			}
-			menuList.appendChild(listItem);
-            console.log(menuItem.item);
-		}
 		}
 	});
 }
