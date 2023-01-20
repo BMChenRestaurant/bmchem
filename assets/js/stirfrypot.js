4ed6d2bea979b9b1742b8b9e3d218831d802eca6
Eddyl112 <edwardlui17@gmail.com>
2023-01-20 01:32:05 -0500

    tab and fix undef

--- assets/js/stirfrypot.js
+++ assets/js/stirfrypot.js
@@ -20,6 +20,7 @@ function parseMenu() {
 			    listItem.innerHTML = "<p class=\"listing\"><strong>" + menuItem.item + " - " + menuItem.price + "</strong></p>";
 			}
 			menuList.appendChild(listItem);
+            console.log(menuItem.item);
 		}
 		}
 	});
